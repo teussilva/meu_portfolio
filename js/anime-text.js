585772fe861cce968432127de26a3b523baea551
Matheus Henrique <mh8093828@gmai.com>
2025-01-12 13:46:15 -0300

    adicionando nova alteracao

--- js/anime-text.js
+++ js/anime-text.js
@@ -4,11 +4,9 @@ const animeTextHeader = text => {
     let arrLetters = text.textContent.split('')
     text.textContent = ''
     arrLetters.forEach((letter, i) =>{
-
         if(letter && i >= 0 && i <= 26){
            setTimeout(() => {
-            console.log(text.textContent += letter)
-            console.log(i)
+            text.textContent += letter
            }, 1500 * i)
         }
     })
